dd0396bc45d9a7ae6f471db21306e206daed3a5f
Alemaogerman <133696366+Alemaogerman@users.noreply.github.com>
2026-05-28 23:06:14 -0300

    Add files via upload

---
+++ Web-Proxy-main/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify, redirect, url_for, render_template
+import json
+import requests
+from urllib.parse import urlparse
+import time
+
+app = Flask(__name__)
+
+# 1) verificar se o site está bloqueado
+# 2) caso não esteja, retornar exatamnte oq teria no site, mas com as palavras filtradas, caso haja
+
+# falta retornar uma página html personalizada caso o site seja bloqueado
+# log de acessos
+
+@app.route("/<path:site>", methods=['POST', 'GET'])
+def filtro(site):
+    with open('blocked.json', 'r') as blocked:
+        blockedFile = json.load(blocked)
+
+        domínio = urlparse(site).netloc
+
+        if site in blockedFile["bloqueados"] or domínio in blockedFile["bloqueados"]:
+            with open("log.txt", "a", encoding="utf-8") as arquivo:
+                arquivo.write(f"{str(time.ctime())}, {site}, bloqueado\n ")
+            return redirect (url_for('bloqueio'))
+
+        else:
+            c=0
+            with open('words.json', 'r') as words:
+                wordsFile = json.load(words)
+
+                siteRequest = requests.get(site)
+                siteTexto = siteRequest.text
+                aaa = siteTexto
+                bk_siteTexto=siteTexto
+
+                for word in wordsFile:
+                    wordLower = word.lower()
+                    siteTextoLower = siteTexto.lower()
+
+                    posição = siteTextoLower.find(wordLower)
+
+                    while posição != -1:
+
+                        siteTexto = siteTexto[:posição] + wordsFile[word] + siteTexto[posição + len(word):]
+                        siteTextoLower = siteTexto.lower()
+                        aaa = posição
+                        posição = siteTextoLower.find(wordLower)
+
+                        if aaa != siteTexto:
+                            if c==0:
+                                with open("log.txt", "a", encoding="utf-8") as arquivo:
+                                    arquivo.write(f"{str(time.ctime())}, {site}, filtrado\n")
+                                c=1
+            if c==0:
+                with open("log.txt", "a", encoding="utf-8") as arquivo:
+                        arquivo.write(f"{str(time.ctime())}, {site}, normal\n")
+            return siteTexto
+@app.route ("/broqueado", methods=['POST', 'GET'])
+def bloqueio():
+    return render_template('saida.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
