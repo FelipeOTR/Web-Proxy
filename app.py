b8a0163b8c1b1a387a4b8d6758650359d7598c04
Alemaogerman <133696366+Alemaogerman@users.noreply.github.com>
2026-06-01 14:58:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, redirect, url_for, render_template
 import json
 import requests
 from urllib.parse import urlparse
+import time
 
 app = Flask(__name__)
 
@@ -19,13 +20,19 @@ def filtro(site):
         domínio = urlparse(site).netloc
 
         if site in blockedFile["bloqueados"] or domínio in blockedFile["bloqueados"]:
-            return f'Site bloqueado' # return render template
+            with open("log.txt", "a", encoding="utf-8") as arquivo:
+                arquivo.write(f"{str(time.ctime())}, {site}, bloqueado\n ")
+            return redirect (url_for('bloqueio'))
+
         else:
+            c=0
             with open('words.json', 'r') as words:
                 wordsFile = json.load(words)
 
                 siteRequest = requests.get(site)
                 siteTexto = siteRequest.text
+                aaa = siteTexto
+                bk_siteTexto=siteTexto
 
                 for word in wordsFile:
                     wordLower = word.lower()
@@ -34,11 +41,24 @@ def filtro(site):
                     posição = siteTextoLower.find(wordLower)
 
                     while posição != -1:
+
                         siteTexto = siteTexto[:posição] + wordsFile[word] + siteTexto[posição + len(word):]
                         siteTextoLower = siteTexto.lower()
+                        aaa = posição
                         posição = siteTextoLower.find(wordLower)
 
+                        if aaa != siteTexto:
+                            if c==0:
+                                with open("log.txt", "a", encoding="utf-8") as arquivo:
+                                    arquivo.write(f"{str(time.ctime())}, {site}, filtrado\n")
+                                c=1
+            if c==0:
+                with open("log.txt", "a", encoding="utf-8") as arquivo:
+                        arquivo.write(f"{str(time.ctime())}, {site}, normal\n")
             return siteTexto
+@app.route ("/broqueado", methods=['POST', 'GET'])
+def bloqueio():
+    return render_template('saida.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
